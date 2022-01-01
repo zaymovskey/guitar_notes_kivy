1e23ca5270874e5248f568376ca7990999e9205c
Sergey Zaimovskii <zaymovskey@mail.ru>
2022-01-01 16:16:29 +0300

    sound recorder

--- main.py
+++ main.py
@@ -1,10 +1,60 @@
 from kivy.app import App
-from kivy.uix.button import Button
+from kivy.lang import Builder
+from kivy.properties import ObjectProperty
+from kivy.uix.boxlayout import BoxLayout
 
 
-class TestApp(App):
+class AudioInterface(BoxLayout):
+    audio = ObjectProperty()
+    has_recording = False
+
+    def start_recording(self):
+        state = self.audio.state
+        if state == 'ready':
+            self.audio.start()
+        if state == 'recording':
+            self.audio.stop()
+            self.has_recording = True
+        self.update_labels()
+
+    def start_playing(self):
+        state = self.audio
+        if state == 'playing':
+            self.audio.stop()
+        else:
+            self.audio.play()
+        self.update_labels()
+
+    def update_labels(self):
+        record_button = self.ids['record_button']
+        play_button = self.ids['play_button']
+        state_label = self.ids['state']
+
+        state = self.audio.state
+        play_button.disabled = not self.has_recording
+
+        state_label.text = 'AudioPlayer State: ' + state
+
+        if state == 'ready':
+            record_button.text = 'START RECORD'
+
+        if state == 'recording':
+            record_button.text = 'STOP RECORD'
+            play_button.disabled = True
+
+        if state == 'playing':
+            play_button.text = 'STOP AUDIO'
+            record_button.disabled = True
+        else:
+            play_button.text = 'PLAY AUDIO'
+            record_button.disabled = False
+
+
+class AudioApp(App):
     def build(self):
-        return Button(text='Hello World')
+        return AudioInterface()
 
 
-TestApp().run()
+if __name__ == '__main__':
+    # run загружает kv файл с названием класса без суффикса App
+    AudioApp().run()
